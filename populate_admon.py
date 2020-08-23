9ffeb23c23ec634eb45651f1f13a8a97227c0e33
map <port02.server@gmail.com>
2020-08-23 18:23:50 +0200

    update version

--- populate_admon.py
+++ populate_admon.py
@@ -113,7 +113,7 @@ def populate(uri, datadir=os.path.dirname(__file__) or os.getcwd()):
     def desc(n: int) -> str:
         return 'Load {0:<' + str(27 - len(str(n))) + 's} ({1}/{2})'
 
-    party_seq, = Sequence.find([('name', '=', 'Party')])
+    (party_seq,) = Sequence.find([('name', '=', 'Party')])
     party_seq.number_next = 10001
     party_seq.save()
 
@@ -253,7 +253,7 @@ def populate(uri, datadir=os.path.dirname(__file__) or os.getcwd()):
             subdivision = next(filter(lambda f: f['id'] == old_id, csvreader), None)
 
         if subdivision and country:
-            new_subdivision, = Subdivision.find([('code', '=', subdivision['code']), ('country', '=', country.id)])
+            (new_subdivision,) = Subdivision.find([('code', '=', subdivision['code']), ('country', '=', country.id)])
 
         if not new_subdivision:
             logging.error('<function get_subdivision>: Subdivision not found id: ' + old_id)
@@ -801,16 +801,22 @@ def populate(uri, datadir=os.path.dirname(__file__) or os.getcwd()):
                         )
                         record.contact_mechanisms.append(_record)
                     elif _row['active'] not in ('f', 0):
-                        logging.error('<party_contact_mechanism>: Active record with null value and value_compact:'
+                        logging.error(
+                            '<party_contact_mechanism>: Active record with null value and value_compact:'
                             + _row['value_compact']
-                            + ' from party with id: ' + row['id']
-                            + ' and name: ' + row['name']
+                            + ' from party with id: '
+                            + row['id']
+                            + ' and name: '
+                            + row['name']
                         )
                     else:
-                        logging.warning('<party_contact_mechanism>: Inactive record with null value and value_compact:'
+                        logging.warning(
+                            '<party_contact_mechanism>: Inactive record with null value and value_compact:'
                             + _row['value_compact']
-                            + ' from party with id: ' + row['id']
-                            + ' and name: ' + row['name']
+                            + ' from party with id: '
+                            + row['id']
+                            + ' and name: '
+                            + row['name']
                         )
 
             seen = set()
@@ -1028,7 +1034,7 @@ def populate(uri, datadir=os.path.dirname(__file__) or os.getcwd()):
             total=rawgencount(path_data_file(datadir, 'condo_unit.csv')) - 1,
         ):
 
-            unit, = CondoUnit.find([('name', '=', row['name']), ('company', '=', idcompany[row['company']])])
+            (unit,) = CondoUnit.find([('name', '=', row['name']), ('company', '=', idcompany[row['company']])])
 
             if unit:
                 with open(path_data_file(datadir, 'condo_unit-factor.csv'), 'r') as _csvfile:
@@ -1037,7 +1043,7 @@ def populate(uri, datadir=os.path.dirname(__file__) or os.getcwd()):
                     for _row in filter(lambda f: f['unit'] == row['id'], _csvreader):
                         for __row in filter(lambda f: f['id'] == _row['condofactor'], table['condo_factor']):
                             company = get_company(__row['company'])
-                            condofactor, = CondoFactor.find(
+                            (condofactor,) = CondoFactor.find(
                                 [('name', '=', __row['name']), ('company', '=', company.id)]
                             )
                         if condofactor:
